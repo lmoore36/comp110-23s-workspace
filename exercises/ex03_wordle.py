b3f3cf46bb42e98004209288f8371ca6657447a2
Lucy Moore <lmoore36@unc.edu>
2023-02-21 21:05:26 -0500

    save 3

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,19 +1,17 @@
 """EX03 - Structured Wordle!"""
 
 __author__ = "730556876"
-guess_word: str = input("Enter a 5 character word:")
-single_letter: str = input("Guess a letter:")
-guess_idx: int = 0
-
 
 def contains_char(guess_word, single_letter):
     """Searches each word for the guessed lettter"""
+    assert len(single_letter) == 1
+    guess_idx: int = 0
     while guess_idx < len(guess_word):
         if single_letter == guess_word[guess_idx]:
             return True
-        guess_idx = guess_idx + 1
-        return False
+        guess_idx == guess_idx + 1
+    return False
     
-
-
-           
+if __name__ == "__main__":
+    guess_word: str = input("Enter a 5 character word:")
+    single_letter: str  = input("Guess a letter:")
