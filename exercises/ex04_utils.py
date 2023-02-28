cfc23aab585d82da978c5bb354984de765f92f56
Lucy Moore <lmoore36@unc.edu>
2023-02-28 15:32:39 -0500

    i think this will work

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,8 +5,8 @@ __author__ = "730556876"
 def all(list: list[int], int: int) -> bool:
     """Searches number list for the example integer"""
     list_idx = 0
-    while list[list_idx] == int:
-        list_idx += 1
-        if list[len(list)-1] == int:
-            return True
-    return False
+    while list_idx < len(list):
+        if list[list_idx] != int:
+           return False
+        list_idx += 1 
+    return True
