56de41ad0ef05913c5ece0f18d7e409f5eee2166
Lucy Moore <lmoore36@unc.edu>
2023-03-08 18:00:46 -0500

    yes

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,6 +2,7 @@
 
 __author__ = "730556876"
 
+
 def only_evens(numbers: list[int]) -> int:
     """Returns even numbers from a list."""
     evens: list[int] = []
@@ -10,25 +11,21 @@ def only_evens(numbers: list[int]) -> int:
             evens.append(x)
     return evens
 
+
 def concat(list1: list[int], list2: list[int]) -> int:
     """Adds two lists together."""
     final_list: list[int] = list1 + list2
     return final_list
 
+
 def sub(list: list[int], idx1: int, idx2: int) -> int:
     """Subsets a list within a range."""
     if idx1 < 0:
         idx1 = 0
-    
     if idx2 > len(list):
         idx2 = len(list)
-    
     subset_list: list[int] = []
-
     while idx1 < idx2:
         subset_list.append(list[idx1])
         idx1 += 1
     return subset_list
-
-      
-
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,51 +3,67 @@
 __author__ = "730556876"
 
 from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import concat
+from exercises.ex05.utils import sub
+
 
 def test_only_evens_use() -> None:
+    """Use case test only_evens."""
     test_list: list[int] = [2, 4, 7, 3, 6]
     assert only_evens(test_list) == [2, 4, 6]
 
+
 def test_only_evens_edge1() -> None:
+    """No list 2."""
     test_list: list[int] = [7, 3]
     assert only_evens(test_list) == []
 
+
 def test_only_evens_edge2() -> None:
+    """No list 1."""
     test_list: list[int] = []
     assert only_evens(test_list) == []
 
-from exercises.ex05.utils import concat
 
 def test_concat_use() -> None:
+    """Use case test concat."""
     test_list1: list[int] = [1, 2, 3]
     test_list2: list[int] = [4, 5, 6]
     assert concat(test_list1, test_list2) == [1, 2, 3, 4, 5, 6]
 
+
 def test_concat_edge1() -> None:
+    """No list 2."""
     test_list1: list[int] = [1, 2, 3]
     test_list2: list[int] = []
     assert concat(test_list1, test_list2) == [1, 2, 3]
 
+
 def test_concat_edge2() -> None:
+    """No list 1."""
     test_list1: list[int] = []
     test_list2: list[int] = [5, 6, 7, 8, 10]
     assert concat(test_list1, test_list2) == [5, 6, 7, 8, 10]
 
-from exercises.ex05.utils import sub
 
 def test_sub_use() -> None:
+    """Use case test sub."""
     test_list: list[int] = [10, 20, 30, 40]
     idx1: int = 1
     idx2: int = 3
     assert sub(test_list, idx1, idx2) == [20, 30]
 
+
 def test_sub_edge1() -> None:
+    """Idx2 larger than length."""
     test_list: list[int] = [10, 20, 30, 40]
     idx1: int = 1
     idx2: int = 5
     assert sub(test_list, idx1, idx2) == [20, 30, 40]
 
+
 def test_sub_edge2() -> None:
+    """Negative Idx1."""
     test_list: list[int] = [10, 20, 30, 40]
     idx1: int = -1
     idx2: int = 3
