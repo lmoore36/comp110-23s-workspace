a955919657899c30d491a36e5692501f904a0d62
Lucy Moore <lmoore36@unc.edu>
2023-04-17 18:17:22 -0400

    commit ate

--- exercises/ex09/fish.py
+++ exercises/ex09/fish.py
@@ -2,7 +2,10 @@
 
 class Fish:
     
+    age: int
+
     def __init__(self):
+        self.age = 0
         return None
     
     def one_day(self):
---
+++ exercises/ex09/river_simulation.py
@@ -0,0 +1,6 @@
+
+from river import River
+
+my_river: River = River (10, 2)
+
+my_river.view_river()
