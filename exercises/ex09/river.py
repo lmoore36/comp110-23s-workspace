09c4900dff94174517252b9cc34edf146f0e7b09
Lucy Moore <lmoore36@unc.edu>
2023-04-18 21:44:27 -0400

    yuh

--- exercises/ex09/river.py
+++ exercises/ex09/river.py
@@ -9,20 +9,44 @@ class River:
     bears: list[Bear]
     fish: list[Fish]
 
-    def __init__(self, num_fish: int, num_bears:int):
+    def __init__(self, num_fish: int, num_bears: int):
         """New River with num_fish Fish and num_bears Bears"""
         self.day: int = 0
-        self.fish: list[Fish] = num_fish
-        self.bears: list[Bear] = num_bears
-        # populate the river with fish and bears
-        #for x in range(0, num_fish):
-            #self.fish.append(Fish())
-        #for x in range(0, num_bears):
-            #self.bears.append(Bear())
+        self.fish: list[Fish] = [num_fish]
+        self.bears: list[Bear] = [num_bears]
+        #populate the river with fish and bears
+        for x in range(0, num_fish):
+            self.fish.append(Fish())
+        for x in range(0, num_bears):
+            self.bears.append(Bear())
+        
+        print(self.fish)
+        print(self.bears)
 
     def check_ages(self):
+        
+        bears_list: list[Bear]
+        fish_list: list[Fish]
+
+        bears_list = self.bears
+        fish_list = self.fish
+        
+        for fish in fish_list: 
+            if Fish.age > 3:
+                fish_list.pop(Fish)
+        print(fish_list)
+        
+        for bear in bears_list:
+            if Bear.age > 5:
+                bears_list.pop(Bear)
+        print(bears_list)
+    
         return None
 
+    def remove_fish(self, amount: int):
+        
+        return None
+    
     def bears_eating(self):
         return None
     
@@ -47,10 +71,10 @@ class River:
         self.day += 1
         # Simulate one day for all Bears
         for bear in self.bears:
-            bear.one_day()
+            Bear.one_day()
         # Simulate one day for all Fish
         for fish in self.fish:
-            fish.one_day()
+            Fish.one_day()
         # Simulate Bear's eating
         self.bears_eating()
         # Remove hungry Bear's from River
